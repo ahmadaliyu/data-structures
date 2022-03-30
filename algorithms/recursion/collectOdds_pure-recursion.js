bcb4ad30315670c4fe41a464708e995dc53760d0
aahmadali <aliyuahmad1996@gmail.com>
2022-03-30 23:24:57 +0100

    collect odds recursion

---
+++ algorithms/recursion/collectOdds_pure-recursion.js
@@ -0,0 +1,17 @@
+//Write a function that collect all odd values in an array
+
+const collectOddValues = (arr) =>{
+    let result = []
+
+      if(arr.length === 0){
+          return result
+      }
+      if(arr[0] % 2 !== 0){
+          result.push(arr[0])
+      }
+      result = result.concat(collectOddValues(arr.slice(1)))
+    
+    return result
+}
+
+console.log(collectOddValues([2,3,4,5,6,7]))
